e86b3c220602a7152b49ef9896e62778484a2230
ElfGod <albertogx@icloud.com>
2023-03-09 02:02:47 -0500

    Section 10: forEach

--- 10-Apply-Functions-to-Collections-of-Data/01.js
+++ 10-Apply-Functions-to-Collections-of-Data/01.js
@@ -2,8 +2,72 @@
 // alert("It's working!");
 
 // ******************************
-// Intro to Array Callback Methods ******************************
+// forEach ******************************
 // Example 1
-/* 
 
-*/
+console.log('\n ');
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27]
+
+numbers.forEach(function (element) {
+  console.log(element)
+})
+
+console.log('\n')
+// Example 2
+numbers.forEach(function (num) {
+  console.log(num * 2)
+})
+
+console.log('\n')
+// Example 3
+function printTriple(n) {
+  console.log(n * 3)
+}
+
+numbers.forEach(printTriple)
+
+// ====================
+const books = [
+  {
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25,
+  },
+  {
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42,
+  },
+  {
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11,
+  },
+  {
+    title: 'A Gentleman in Moscow',
+    authors: ['Amor Towles'],
+    rating: 4.36,
+  },
+]
+
+console.log('\n')
+// Example 4
+books.forEach(function (book) {
+  console.log(book.title.toUpperCase())
+})
+
+console.log('\n')
+for (let book of books) {
+  console.log(book.title.toUpperCase())
+}
+
+console.log('\n')
+for (let i = 0; i < books.length; i++) {
+  console.log(books[i].title.toUpperCase())
+}
+
+console.log('\n')
+// printing the Index of numbers
+numbers.forEach(function(num, index) {
+  console.log(index, num)
+})
--- 10-Apply-Functions-to-Collections-of-Data/README.md
+++ 10-Apply-Functions-to-Collections-of-Data/README.md
@@ -17,3 +17,24 @@
   - reduce
   - some
   - every
+
+## 2. forEach
+
+Accepts a callback function.  
+Calls the function once per element in the array.
+
+```
+const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+nums.forEach(function (n)) {
+  console.log(n * n); // prints: 81, 64, 49, 36, 25, 16, 9, 4, 1
+});
+
+nums.forEach(function (el)) {
+  if (el % 2 === 0) {
+    console.log(el) // prints: 8, 6, 4, 6
+  }
+});
+```
+
+**We can also add a second a parameter to our callback to the function if we want to use the index**
