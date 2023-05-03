887ebceedd16e79695b49e855b67d06aae3330b6
ElfGod <albertogx@icloud.com>
2023-05-03 18:57:38 -0500

    The Async Keyword

---
+++ 18-Async-Await-JS-Magic/02/app.js
@@ -0,0 +1,84 @@
+// ******************************
+// The Async Keyword ******************************
+// Example 1
+
+console.log('The Async Keyword')
+
+//   const data = axios.get('https://swapi.dev/api/planets/')
+
+// Run the functions in the Browser console
+// function greet() {
+//   return 'HELLO!!!'
+// }
+// greet()
+
+// Whatever the value that i'm returning is, the promise that will
+// be returned from greetAsync() will be resolved with that value
+// async function greetAsync() {
+//   return 'HELLO async!!!'
+// }
+
+// greetAsync().then((val) => {
+//   console.log('PROMISE RESOLVED WITH: ', val);
+// })
+
+// Example 2
+// Run the functions in the Browser console
+// async function add(x, y) {
+//   return x + y
+// }
+// add()
+
+/*
+How do we return a promise that is not resolved?
+
+- If we want to return a rejected promise, all that we do is 
+raise an exeption. So if we throw an exception, we throw an error,
+that promise will be rejected
+*/
+async function add(x, y) {
+  if(typeof x !== 'number' || typeof y !== 'number') {
+    throw ' X and Y must be numbers!'
+  }
+  return x + y
+}
+add(3, 2)
+add(3, 'a')
+add('e', 'r').then((val) => {
+  console.log('PROMISE RESOLVED WITH: ', val);
+}).catch((err) => {
+  console.log('PROMISE REJECTED WITH: ', err)
+})
+
+console.log('\n');
+add(2, 3)
+  .then((val) => {
+    console.log('PROMISE RESOLVED WITH: ', val)
+  })
+  .catch((err) => {
+    console.log('PROMISE REJECTED WITH: ', err)
+  })
+
+// Example 3
+// Manual Promise creation
+// function add(x, y) {
+//   return new Promise((resolve, rejected) => {
+//     if (typeof x !== 'number' || typeof y !== 'number') {
+//       rejected(' X and Y must be numbers!')
+//     }
+//     resolve(x + y)
+//   })
+// }
+
+// add(6, 7).then((val) => {
+//     console.log('PROMISE RESOLVED WITH: ', val)
+// }).catch((err) => {
+//       console.log('PROMISE REJECTED WITH: ', err)
+// })
+// add('r', 7)
+//   .then((val) => {
+//     console.log('PROMISE RESOLVED WITH: ', val)
+//   })
+//   .catch((err) => {
+//     console.log('PROMISE REJECTED WITH: ', err)
+//   })
--- 18-Async-Await-JS-Magic/README.md
+++ 18-Async-Await-JS-Magic/README.md
@@ -10,3 +10,35 @@ Async functions depend extremely heavily on promises. Async functions are just a
 
 - ASYNC
 - AWAIT
+
+## 2. The Async Keyword
+
+#### The async keyword
+- Async functions always return a promise
+- If the function returns a value, the promise will be resolved with that value
+- If the function throws an exception, the promise will be rejected
+
+```javascript
+async function hello() {
+  return 'Hey guy!'
+}
+hello()
+// Promise {<resolved>: "Hey guy!"}
+async function uhOh() {
+  throw new Error('oh no!')
+}
+uhOh()
+// Promise {<rejected>: Error: oh no!}
+```
+So we use async in front of a function declaration or function expression, we use it to designate a certain function as an async function.
+
+The key thing to understand about this keyword is when you put it in front of a function, this function behaves differently. **It's now going to return a promise**
+
+So even though i never write a new promise, i don't explicitly say, hey, return a promise. I'm simply saying return a string because i put async there the function now returns a promise.
+
+#### How do we return a promise that is not resolved?
+
+- If we want to return a rejected promise, all that we do is 
+raise an exeption. So if we throw an exception, we throw an error,
+that promise will be rejected
+
