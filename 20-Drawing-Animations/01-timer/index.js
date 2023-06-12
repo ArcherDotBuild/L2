29201a60b366be542adbcf16f4d73f7fb57573b9
ElfGod <albertogx@icloud.com>
2023-06-11 21:24:07 -0500

    Section 20: Binding Events in a Class

---
+++ 20-Drawing-Animations/01-timer/index.js
@@ -0,0 +1,24 @@
+// ******************************
+// What on Earth are Prototypes ******************************
+
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput
+    this.startButton = startButton
+    this.pauseButton = pauseButton
+
+    // start(){}, Calling the constructor anytime the user clicks on the start button
+    this.startButton,addEventListener('click', this.start)
+  }  
+
+  start() {
+    console.log('Time to start the timer!');
+  }
+}
+
+const durationInput = document.querySelector('#duration')
+const startButton = document.querySelector('#pause')
+const pauseButton= document.querySelector('#pause')
+
+// Create our instance of the timer and pass in those three elements
+const timer = new Timer(durationInput, startButton, pauseButton)
---
+++ 20-Drawing-Animations/README.md
@@ -0,0 +1,5 @@
+## 19-Prototypes-Classes-And-The-New-Operator
+
+## 1. 06 Binding Events in a Class
+
+## 
