8df3a464b4e7777d6292c93d88b339f587e3c040
ElfGod <albertogx@icloud.com>
2023-08-02 01:13:20 -0500

    Section 23: Handling Form Submission

--- 23-Make-a-Secret-Message-Sharing-App/01-Message/index.js
+++ 23-Make-a-Secret-Message-Sharing-App/01-Message/index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+// console.log('hello world');
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault()
+
+  const input = document.querySelector('input')
+
+  console.log(input.value);
+})
