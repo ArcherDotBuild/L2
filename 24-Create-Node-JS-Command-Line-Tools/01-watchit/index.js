97f5716142daea048c021ab977e8ef13c72e844c
ElfGod <albertogx@icloud.com>
2023-08-07 00:00:50 -0500

    Section 24: The Require Cache

--- 24-Create-Node-JS-Command-Line-Tools/01-watchit/index.js
+++ 24-Create-Node-JS-Command-Line-Tools/01-watchit/index.js
@@ -1,8 +1,5 @@
-const message = require('./myscript.js')
+// const message = require('./myscript.js')
+// console.log(message)
 
-console.log(message)
-console.log(arguments);
-console.log(require);
-console.log(module);
-console.log(__filename);
-console.log(__dirname);
+require('./myscript.js')
+console.log(require.cache);
