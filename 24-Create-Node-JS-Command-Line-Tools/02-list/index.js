2178bb101ee6882a50e1e36aa1c4c57b8858e1a8
ElfGod <albertogx@icloud.com>
2023-08-10 23:34:31 -0500

    Section 24: The Callback Pattern in Node

--- 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
+++ 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
@@ -1,1 +1,17 @@
 const fs = require('fs')
+
+fs.readdir('.', (err, filenames) => {
+  // EITHER
+  // err === an error object, which means something went wrong
+  // OR
+  // err === null, which means everything is OK
+
+  if(err) {
+    // error handling code here
+    
+    console.log(err);
+    // return
+  }
+
+  console.log(filenames);
+})
