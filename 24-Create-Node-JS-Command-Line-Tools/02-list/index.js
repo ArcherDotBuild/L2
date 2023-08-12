de36e3bc2c3e34f43b4bd6e6bcd1f36e64108bab
ElfGod <albertogx@icloud.com>
2023-08-11 23:58:08 -0500

    Section 24: The Process.cwd Function

--- 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
+++ 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
@@ -1,17 +1,17 @@
 const fs = require('fs')
 
-fs.readdir('.', (err, filenames) => {
+fs.readdir(process.cwd(), (err, filenames) => {
   // EITHER
   // err === an error object, which means something went wrong
   // OR
   // err === null, which means everything is OK
 
-  if(err) {
+  if (err) {
     // error handling code here
-    
-    console.log(err);
+
+    console.log(err)
     // return
   }
 
-  console.log(filenames);
+  console.log(filenames)
 })
