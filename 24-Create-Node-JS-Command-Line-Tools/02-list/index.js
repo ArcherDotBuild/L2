ab12917666a1cdfbb334d1ae0636e81ff6ad097d
ElfGod <albertogx@icloud.com>
2023-08-13 23:23:16 -0500

    Section 24: A Buggy Initial Implementation

--- 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
+++ 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
@@ -4,17 +4,19 @@
 const fs = require('fs')
 
 fs.readdir(process.cwd(), (err, filenames) => {
-  // EITHER
-  // err === an error object, which means something went wrong
-  // OR
-  // err === null, which means everything is OK
-
   if (err) {
-    // error handling code here
-
     console.log(err)
-    // return
   }
+  // console.log(filenames)
 
-  console.log(filenames)
+  // BAD CODE HERE!!!!
+  for(let filename of filenames) {
+    fs.lstat(filename, (err, stats) => {
+      if(err) {
+        console.log(err);
+      }
+
+      console.log(filename, stats.isFile());
+    })
+  }
 })
