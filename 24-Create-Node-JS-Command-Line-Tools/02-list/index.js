96adba66c6a141f75853be4edc3184c3d297739c
ElfGod <albertogx@icloud.com>
2023-08-14 23:46:22 -0500

    Section 24: A Callback-Based Solution

--- 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
+++ 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
@@ -9,14 +9,31 @@ fs.readdir(process.cwd(), (err, filenames) => {
   }
   // console.log(filenames)
 
-  // BAD CODE HERE!!!!
+  // Creates an array with a length equal to the filenames array
+  // and every value or index will be filled with the value of null
+  const allStats = Array(filenames.length).fill(null)
+
   for(let filename of filenames) {
+    const index = filenames.indexOf(filename)
+
     fs.lstat(filename, (err, stats) => {
       if(err) {
         console.log(err);
       }
 
-      console.log(filename, stats.isFile());
+      allStats[index] = stats
+
+      // every() func is built into every single array
+      const ready = allStats.every((stats) => {
+        return stats
+      })
+
+      if(ready) {
+        allStats.forEach((stats, index) => {
+          // console.log(filename);
+          console.log(filenames[index], stats.isFile());
+        })
+      }
     })
   }
 })
