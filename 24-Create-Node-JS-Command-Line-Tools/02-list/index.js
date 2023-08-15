fd6dbbe43d4b637ed3c8921e54c7b13a04e0d5a7
ElfGod <albertogx@icloud.com>
2023-08-15 00:44:08 -0500

    Section 24: Callback-Based Functions Using Promises

--- 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
+++ 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
@@ -2,38 +2,29 @@
 // Allow us to run it like an executable
 
 const fs = require('fs')
+const util = require('util')
+
+// Method #2
+// const lstat = util.promisify(fs.lstat)
+
+// Method #3
+const { lstat } = fs.promises
 
 fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
     console.log(err)
   }
-  // console.log(filenames)
-
-  // Creates an array with a length equal to the filenames array
-  // and every value or index will be filled with the value of null
-  const allStats = Array(filenames.length).fill(null)
-
-  for(let filename of filenames) {
-    const index = filenames.indexOf(filename)
-
-    fs.lstat(filename, (err, stats) => {
-      if(err) {
-        console.log(err);
-      }
-
-      allStats[index] = stats
-
-      // every() func is built into every single array
-      const ready = allStats.every((stats) => {
-        return stats
-      })
-
-      if(ready) {
-        allStats.forEach((stats, index) => {
-          // console.log(filename);
-          console.log(filenames[index], stats.isFile());
-        })
-      }
-    })
-  }
 })
+
+// Method #1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if(err) {
+//         reject(err)
+//       }
+
+//       resolve(stats)
+//     })
+//   })
+// }
