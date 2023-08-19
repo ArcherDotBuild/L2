f9f0938c4bfa2b5b01cc4209bdfb00fdd28c658c
ElfGod <albertogx@icloud.com>
2023-08-19 00:19:38 -0500

    Section 24: Console Logs with Colors

--- 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
+++ 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
@@ -3,6 +3,7 @@
 
 const fs = require('fs')
 const util = require('util')
+const chalk = require('chalk')
 
 // Method #2
 // const lstat = util.promisify(fs.lstat)
@@ -24,7 +25,11 @@ fs.readdir(process.cwd(), async (err, filenames) => {
   for(let stats of allStats) {
     const index = allStats.indexOf(stats)
 
-    console.log(filenames[index], stats.isFile());
+    if(stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.bold(filenames[index]));
+    }
   }
 })
 
