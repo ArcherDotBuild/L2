0e33ce46194d1764bf8bec83dabf1c676efc537f
ElfGod <albertogx@icloud.com>
2023-08-19 00:11:21 -0500

    Section 24: Promise.all-Based Solution

--- 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
+++ 24-Create-Node-JS-Command-Line-Tools/02-list/index.js
@@ -8,21 +8,23 @@ const util = require('util')
 // const lstat = util.promisify(fs.lstat)
 
 // Method #3
-// const { lstat } = fs.promises
+const { lstat } = fs.promises
 
 fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err)
   }
 
-  for (let filename of filenames) {
-    try {
-      const stats = await lstat(filename)
+  const statPromises = filenames.map((filename) => {
+    return lstat(filename)
+  })
 
-      console.log(filename, stats.isFile())
-    } catch (err) {
-      console.log(err)
-    }
+  const allStats = await Promise.all(statPromises)
+
+  for(let stats of allStats) {
+    const index = allStats.indexOf(stats)
+
+    console.log(filenames[index], stats.isFile());
   }
 })
 
