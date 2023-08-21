3431a799751b9383632a80dab72ed24323c87171
ElfGod <albertogx@icloud.com>
2023-08-20 23:53:22 -0500

    Section 25: Watching Files with Chokidar

--- 25-Create-Your-Own-Project-Runner/01-watchit/index.js
+++ 25-Create-Your-Own-Project-Runner/01-watchit/index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('hello')
+const chokidar = require('chokidar')
+
+chokidar
+  .watch('.')
+  .on('add', () => console.log('FILE ADDED'))
+  .on('change', () => console.log('FILE CHANGED'))
+  .on('unlink', () => console.log('FILE UNLINKED'))
