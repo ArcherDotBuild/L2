a3475b99e04ba87fe5de8dabe63b4a9480b10ff3
ElfGod <albertogx@icloud.com>
2023-08-21 00:03:17 -0500

    Section 25: Debouncing Add Events

--- 25-Create-Your-Own-Project-Runner/01-watchit/index.js
+++ 25-Create-Your-Own-Project-Runner/01-watchit/index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce')
 const chokidar = require('chokidar')
+
+const start = debounce(() => {
+  console.log('STARTING USERS PROGRAM');
+}, 100)
 
 chokidar
   .watch('.')
-  .on('add', () => console.log('FILE ADDED'))
+  .on('add', start)
   .on('change', () => console.log('FILE CHANGED'))
   .on('unlink', () => console.log('FILE UNLINKED'))
