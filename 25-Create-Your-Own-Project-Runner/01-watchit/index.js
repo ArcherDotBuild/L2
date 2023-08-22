d47d29d6c1521c1fe2b34eddbb32559442e93ceb
ElfGod <albertogx@icloud.com>
2023-08-21 20:41:17 -0500

    Section 25: Ensuring Files Exist

--- 25-Create-Your-Own-Project-Runner/01-watchit/index.js
+++ 25-Create-Your-Own-Project-Runner/01-watchit/index.js
@@ -3,22 +3,26 @@
 const debounce = require('lodash.debounce')
 const chokidar = require('chokidar')
 const program = require('caporal')
+const fs = require('fs')
 
 program
   .version('0.0.1')
   .argument('[filename]', 'Name of a file to execute')
-  .action((args) => {
-    console.log(args)
+  .action(async ({ filename }) => {
+    // console.log(args)
+    
+    const name = filename || 'filename.js'
+    try {
+      await fs.promises.access(name)
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`)
+    }
+
+    const start = debounce(() => {
+      console.log('STARTING USERS PROGRAM')
+    }, 100)
+
+    chokidar.watch('.').on('add', start).on('change', start).on('unlink', start)
   })
 
-  program.parse(process.argv)
-
-// const start = debounce(() => {
-//   console.log('STARTING USERS PROGRAM')
-// }, 100)
-
-// chokidar
-//   .watch('.')
-//   .on('add', start)
-//   .on('change', () => console.log('FILE CHANGED'))
-//   .on('unlink', () => console.log('FILE UNLINKED'))
+program.parse(process.argv)
