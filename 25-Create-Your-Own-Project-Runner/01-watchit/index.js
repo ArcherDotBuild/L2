6874affe0d2785d895018bee253627798474b1ae
ElfGod <albertogx@icloud.com>
2023-08-21 20:27:06 -0500

    Section 25: Handling CLI Tools with Caporal

--- 25-Create-Your-Own-Project-Runner/01-watchit/index.js
+++ 25-Create-Your-Own-Project-Runner/01-watchit/index.js
@@ -2,13 +2,23 @@
 
 const debounce = require('lodash.debounce')
 const chokidar = require('chokidar')
+const program = require('caporal')
 
-const start = debounce(() => {
-  console.log('STARTING USERS PROGRAM');
-}, 100)
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action((args) => {
+    console.log(args)
+  })
 
-chokidar
-  .watch('.')
-  .on('add', start)
-  .on('change', () => console.log('FILE CHANGED'))
-  .on('unlink', () => console.log('FILE UNLINKED'))
+  program.parse(process.argv)
+
+// const start = debounce(() => {
+//   console.log('STARTING USERS PROGRAM')
+// }, 100)
+
+// chokidar
+//   .watch('.')
+//   .on('add', start)
+//   .on('change', () => console.log('FILE CHANGED'))
+//   .on('unlink', () => console.log('FILE UNLINKED'))
