0acfab43b5bc9ef990ac5c8a08ec7a50ca2ff1fd
ElfGod <albertogx@icloud.com>
2023-08-29 22:33:56 -0500

    Section 27; Small Refactor

--- 27-Design-a-Custom-Database/01-database/repositories/users.js
+++ 27-Design-a-Custom-Database/01-database/repositories/users.js
@@ -20,23 +20,20 @@ class UsersRepository {
 
   async getAll() {
     // Open the file called this.filename
-    const contents = await fs.promises.readFile(this.filename, {
-      encoding: 'utf8',
-    })
-
-    // Read its contents
-    console.log(contents)
-
-    // Parse the contents
-
-    // Return the parsed data
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8',
+      })
+    )
   }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json')
 
-  await repo.getAll()
+  const users = await repo.getAll()
+
+  console.log(users)
 }
 
 test()
