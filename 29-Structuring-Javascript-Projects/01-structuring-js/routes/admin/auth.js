9c7014b31dde893155c8da3579e3a483470a4e1b
ElfGod <albertogx@icloud.com>
2023-09-24 23:55:51 -0500

    Section 29: Receiving Validation Output

--- 29-Structuring-Javascript-Projects/01-structuring-js/routes/admin/auth.js
+++ 29-Structuring-Javascript-Projects/01-structuring-js/routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { check } = require('express-validator')
+const { check, validationResult } = require('express-validator')
 const usersRepo = require('../../repositories/users')
 const signupTemplate = require('../../views/admin/auth/signup')
 const signinTemplate = require('../../views/admin/auth/signin')
@@ -16,12 +16,15 @@ router.get('/signup', (req, res) => {
 router.post(
   '/signup',
   [
-    check('email').isEmail(),
-    check('password'),
-    check('passwordConfirmation')
+    check('email').trim().normalizeEmail().isEmail(),
+    check('password').trim().isLength({ min: 4, max: 20 }),
+    check('passwordConfirmation').trim().isLength({ min: 4, max: 20 }),
   ],
   async (req, res) => {
     console.log(req.body)
+    // Get access to all that validation logic in the req object
+    const errors = validationResult(req)
+    console.log(errors);
     const { email, password, passwordConfirmation } = req.body
 
     // Check the email if the user has signed up before
