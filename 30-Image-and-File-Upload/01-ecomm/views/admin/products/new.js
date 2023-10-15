9ad209430856916028f70a1ba40f499eaa397fff
ElfGod <albertogx@icloud.com>
2023-10-14 22:36:55 -0500

    Section 30: Better Styling

--- 30-Image-and-File-Upload/01-ecomm/views/admin/products/new.js
+++ 30-Image-and-File-Upload/01-ecomm/views/admin/products/new.js
@@ -4,14 +4,32 @@ const { getError } = require('../../helpers')
 module.exports = ({ errors }) => {
   return layout({
     content: `
-      <form method="POST" enctype="multipart/form-data">
-        <input placeholder="Title" name="title" />
-        ${getError(errors, 'title')}
-        <input placeholder="Price" name="price" />
-        ${getError(errors, 'price')}
-        <input type="file" name="image" />
-        <button>Submit</button>
-      </form>
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Create a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Title" name="title">
+              <p class="help is-danger">${getError(errors, 'title')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
+        </div>
+      </div>
     `,
   })
 }
