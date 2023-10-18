8c88a562de1b8e60dfc00203f71611ed5decb52e
ElfGod <albertogx@icloud.com>
2023-10-18 00:22:30 -0500

    Section 30: Template Update

--- 30-Image-and-File-Upload/01-ecomm/views/admin/products/index.js
+++ 30-Image-and-File-Upload/01-ecomm/views/admin/products/index.js
@@ -4,15 +4,43 @@ module.exports = ({ products }) => {
   const renderedProducts = products
     .map((product) => {
       return `
-      <div>${product.title}</div>
+      <tr>
+        <td>${product.title}</td>
+        <td>${product.price}</td>
+        <td>
+          <a href="">
+            <button class="button is-link">
+              Edit
+            </button>
+          </a>
+        </td>
+        <td>
+          <button class="button is-danger">Delete</button>
+        </td>
+      </tr>
     `
     })
     .join('')
 
   return layout({
     content: `
-      <h1 class="title">Products</h1>
-      ${renderedProducts}
+      <div class="control">
+        <h1 class="subtitle">Products</h1>  
+        <a href="/admin/products/new" class="button is-primary">New Product</a>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${renderedProducts}
+        </tbody>
+      </table>
     `,
   })
 }
