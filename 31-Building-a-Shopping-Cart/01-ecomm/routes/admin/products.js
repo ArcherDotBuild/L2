b369b81f52d5755ad2dcde0a1cb971703bd6aef5
ElfGod <albertogx@icloud.com>
2023-10-23 23:56:20 -0500

    Section 31: Deleting Products

--- 31-Building-a-Shopping-Cart/01-ecomm/routes/admin/products.js
+++ 31-Building-a-Shopping-Cart/01-ecomm/routes/admin/products.js
@@ -74,4 +74,10 @@ router.post(
   }
 )
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+  await productsRepo.delete(req.params.id)
+
+  res.redirect('/admin/products')
+})
+
 module.exports = router
--- 31-Building-a-Shopping-Cart/01-ecomm/views/admin/products/index.js
+++ 31-Building-a-Shopping-Cart/01-ecomm/views/admin/products/index.js
@@ -15,7 +15,9 @@ module.exports = ({ products }) => {
           </a>
         </td>
         <td>
-          <button class="button is-danger">Delete</button>
+          <form method="POST" action="/admin/products/${product.id}/delete">
+            <button class="button is-danger">Delete</button>
+          </form>
         </td>
       </tr>
     `
