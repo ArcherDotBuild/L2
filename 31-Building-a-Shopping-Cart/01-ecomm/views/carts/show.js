4f2dca88be7603ea0d29e6a6cc9d55b2425b8b43
ElfGod <albertogx@icloud.com>
2023-11-02 23:44:35 -0500

    Section 31: Rendering the List

--- 31-Building-a-Shopping-Cart/01-ecomm/views/carts/show.js
+++ 31-Building-a-Shopping-Cart/01-ecomm/views/carts/show.js
@@ -4,14 +4,14 @@ module.exports = ({ items }) => {
   const renderedItems = items
     .map((item) => {
       return `
-      <div>${item.product.title} - ${item.product.price}</div>
+      <div>${item.product.title} - ${item.product.price} - ${item.quantity}</div>
     `
     })
     .join('')
 
   return layout({
     content: `
-    <h1>Cart</h1>
+    <h1>Cart Items</h1>
     ${renderedItems}
     `,
   })
