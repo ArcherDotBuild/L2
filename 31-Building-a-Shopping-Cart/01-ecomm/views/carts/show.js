2d3c9e8d5e8f32426056114afca8fa72be9bbf1c
ElfGod <albertogx@icloud.com>
2023-11-03 23:40:48 -0500

    Section 31: Totaling Cart Items

--- 31-Building-a-Shopping-Cart/01-ecomm/views/carts/show.js
+++ 31-Building-a-Shopping-Cart/01-ecomm/views/carts/show.js
@@ -1,18 +1,65 @@
 const layout = require('../layout')
 
 module.exports = ({ items }) => {
+  // Option 1
+  // let totalPrice = 0
+  // for(let item of items) {
+  //   totalPrice += item.quantity * item.product.price
+  // }
+
+  // Option 2 using reduce
+  const totalPrice = items.reduce((prev, item) => {
+    return (prev + item.quantity * item.product.price)
+  }, 0)
+
   const renderedItems = items
     .map((item) => {
       return `
-      <div>${item.product.title} - ${item.product.price} - ${item.quantity}</div>
-    `
+        <div class="cart-item message">
+          <h3 class="subtitle">${item.product.title}</h3>
+          <div class="cart-right">
+            <div>
+              $${item.product.price}  X  ${item.quantity} = 
+            </div>
+            <div class="price is-size-4">
+              $${item.product.price * item.quantity}
+            </div>
+            <div class="remove">
+              <form method="POST">
+                <button class="button is-danger">                  
+                  <span class="icon is-small">
+                    <i class="fas fa-times"></i>
+                  </span>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      `
     })
     .join('')
 
   return layout({
     content: `
-    <h1>Cart Items</h1>
-    ${renderedItems}
+      <div id="cart" class="container">
+        <div class="columns">
+          <div class="column"></div>
+          <div class="column is-four-fifths">
+            <h3 class="subtitle"><b>Shopping Cart</b></h3>
+            <div>
+              ${renderedItems}
+            </div>
+            <div class="total message is-info">
+              <div class="message-header">
+                Total
+              </div>
+              <h1 class="title">$${totalPrice}</h1>
+              <button class="button is-primary">Buy</button>
+            </div>
+          </div>
+          <div class="column"></div>
+        </div>
+      </div>
     `,
   })
 }
