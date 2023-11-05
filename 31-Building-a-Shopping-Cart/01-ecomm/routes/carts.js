5fdc83ce45663d348c63c0161f8e0139b95b71f4
ElfGod <albertogx@icloud.com>
2023-11-05 00:30:50 -0500

    Section 31: Redirect on Remove

--- 31-Building-a-Shopping-Cart/01-ecomm/routes/carts.js
+++ 31-Building-a-Shopping-Cart/01-ecomm/routes/carts.js
@@ -48,30 +48,39 @@ router.post('/cart/products', async (req, res) => {
     items: cart.items,
   })
 
-  res.send('Product added to cart')
+  // res.send('Product added to cart')
+  res.redirect('/cart')
 })
 
 // Receive a GET request to show all items in cart
 router.get('/cart', async (req, res) => {
-  if(!req.session.cartId) {
+  if (!req.session.cartId) {
     return res.redirect('/')
   }
 
   const cart = await cartsRepo.getOne(req.session.cartId)
 
-  for(let item of cart.items) {
+  for (let item of cart.items) {
     // item === { id: , quantity: }
     const product = await productsRepo.getOne(item.id)
 
     item.product = product
   }
 
-  res.send(cartShowTemplate({ items: cart.items}))
+  res.send(cartShowTemplate({ items: cart.items }))
 })
 
 // Receive a POST request to delete an item from a cart
 router.post('/cart/products/delete', async (req, res) => {
-  console.log(req.body.itemId);
+  // console.log(req.body.itemId);
+  const { itemId } = req.body
+  const cart = await cartsRepo.getOne(req.session.cartId)
+
+  const items = cart.items.filter((item) => item.id !== itemId)
+
+  await cartsRepo.update(req.session.cartId, { items })
+
+  res.redirect('/cart')
 })
 
 module.exports = router
