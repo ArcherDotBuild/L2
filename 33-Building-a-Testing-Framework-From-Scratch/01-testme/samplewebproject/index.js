d038b75b6c61b67adeb7fb8b2ea4d5adc6ba85c7
ElfGod <albertogx@icloud.com>
2023-11-27 22:50:21 -0500

    Section 33: A Sample Web App

---
+++ 33-Building-a-Testing-Framework-From-Scratch/01-testme/samplewebproject/index.js
@@ -0,0 +1,14 @@
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const { value } = document.querySelector('input')
+
+  const header = document.querySelector('h1')
+  if(value.includes('@')) {
+    //  must be valid
+    header.innerHTML = 'Looks good! '
+  } else {
+    // must be invalid
+    header.innerHTML = 'Invalid email'
+  }
+})
